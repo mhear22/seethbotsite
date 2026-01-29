23b357ecd96e61347c36f819cbd6728e3de68c3e
root <root@moltbot.taila60821.ts.net>
2026-01-29 22:56:31 +1000

    Fix ESLint config for ES6 modules in components directory

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,40 @@
+import globals from "globals";
+
 export default [
   {
-    files: ["**/*.js"],
+    ignores: ["eslint.config.js"]
+  },
+  {
+    files: ["components/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        document: "readonly",
+        window: "readonly",
+        console: "readonly",
+        setTimeout: "readonly",
+        setInterval: "readonly",
+        requestAnimationFrame: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "warn",
+      "semi": ["warn", "always"],
+      "quotes": ["warn", "single"],
+      "indent": ["warn", 2],
+      "no-console": "off"
+    }
+  },
+  {
+    files: ["*.js", "server/**/*.js"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "script",
       globals: {
+        ...globals.browser,
         document: "readonly",
         window: "readonly",
         console: "readonly",
