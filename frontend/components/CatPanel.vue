193c3caf620d10f420647b5e1fddc180ed72cb6b
root <root@moltbot.taila60821.ts.net>
2026-02-03 23:11:14 +1000

    Embed Orlando's OTS-01 game in Cats section

--- frontend/components/CatPanel.vue
+++ frontend/components/CatPanel.vue
@@ -25,6 +25,17 @@ const toggle = () => {
       <img v-if="!loading" :src="catImage" class="cat-image" alt="Random cat" />
       <div v-if="loading" class="cat-loading">Loading... 🐱</div>
       <button class="cute-btn" @click="$emit('new-cat')" :disabled="loading">🔄 New Cat</button>
+      
+      <div class="cat-game-container">
+        <iframe 
+          src="https://itch.io/embed-game/3165293" 
+          width="100%" 
+          height="500" 
+          frameborder="0"
+          class="cat-game-iframe">
+          <a href="https://bellicapelli.itch.io/ots-01">OTS-01 by bellicapelli</a>
+        </iframe>
+      </div>
     </div>
   </div>
 </template>
