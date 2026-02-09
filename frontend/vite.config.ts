a69ace6baea9055fc0c9e7bb80388ae89b6696fc
root <root@moltbot.taila60821.ts.net>
2026-02-10 09:08:48 +1000

    Fix: Add @ alias to vite config for #192 build failure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './')
+    }
+  },
   plugins: [
     vue()
   ],
